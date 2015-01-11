0934ddec246097b50e658d1d3366d81415a15b17
dobrybajer <kamil.zakk@gmail.com>
2015-01-11 19:00:49 +0100

    ready to tests 2

--- x/tests.m
+++ x/tests.m
@@ -2,11 +2,11 @@
 TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 7, 'iloscCech', 5, 'iloscPowtorzenWKlasie', 50, 'minLos', 0, 'maxLos', 20, 'zaburzenie', 0.2, 'procRozmTest', 40, 'dyskretyzacja', 10, 'PSOiter', 50, 'PSOs', 16, 'sciezkaOutputKlas', 'C:\Users\Kamil\Desktop\wynik1.xlsx')
 
 %Kamil
-TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 5, 'iloscCech', 40, 'iloscPowtorzenWKlasie', 150, 'minLos', 0, 'maxLos', 5, 'zaburzenie', 1, 'dyskretyzacja', 5, 'PSOiter', 700, 'PSOs', 16, 'sciezkaOutputKlas', '..\rozpoznane_a1_1.xlsx', 'sciezkaOutputErr', '..\blad_a1_1.xlsx')
+TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 5, 'iloscCech', 40, 'iloscPowtorzenWKlasie', 150, 'minLos', 0, 'maxLos', 5, 'zaburzenie', 0.5, 'dyskretyzacja', 5, 'PSOiter', 700, 'PSOs', 16, 'sciezkaOutputKlas', '..\rozpoznane_a1_1.xlsx', 'sciezkaOutputErr', '..\blad_a1_1.xlsx')
 %Pawe³
-TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 10, 'iloscCech', 10, 'iloscPowtorzenWKlasie', 100, 'minLos', 0, 'maxLos', 10, 'zaburzenie', 1,  'dyskretyzacja', 10, 'PSOiter', 900, 'PSOs', 12, 'sciezkaOutputKlas', '..\rozpoznane_a1_2.xlsx', 'sciezkaOutputErr', '..\blad_a1_2.xlsx')
+TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 10, 'iloscCech', 10, 'iloscPowtorzenWKlasie', 100, 'minLos', 0, 'maxLos', 10, 'zaburzenie', 1.2,  'dyskretyzacja', 10, 'PSOiter', 900, 'PSOs', 12, 'sciezkaOutputKlas', '..\rozpoznane_a1_2.xlsx', 'sciezkaOutputErr', '..\blad_a1_2.xlsx')
 %£ukasz
-TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 20, 'iloscCech', 20, 'iloscPowtorzenWKlasie', 50, 'minLos', 0, 'maxLos', 20, 'zaburzenie', 1,  'dyskretyzacja', 8, 'PSOiter', 500, 'PSOs', 8, 'sciezkaOutputKlas', '..\rozpoznane_a1_3.xlsx', 'sciezkaOutputErr', '..\blad_a1_3.xlsx')
+TAIO2014_G04('etap', 'a1', 'wejscieTyp', 'gen', 'iloscKlas', 20, 'iloscCech', 20, 'iloscPowtorzenWKlasie', 50, 'minLos', 0, 'maxLos', 20, 'zaburzenie', 2.5,  'dyskretyzacja', 8, 'PSOiter', 500, 'PSOs', 8, 'sciezkaOutputKlas', '..\rozpoznane_a1_3.xlsx', 'sciezkaOutputErr', '..\blad_a1_3.xlsx')
 
 
 %2:
